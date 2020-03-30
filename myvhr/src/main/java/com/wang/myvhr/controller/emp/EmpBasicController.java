9b9e87ad6d0dfbe8489a9f65dc3742fdcfba328d
wanghuan <2578145258@qq.com>
2020-03-30 23:54:25 +0800

    2020-3-29

--- myvhr/src/main/java/com/wang/myvhr/controller/emp/EmpBasicController.java
+++ myvhr/src/main/java/com/wang/myvhr/controller/emp/EmpBasicController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
@@ -31,6 +32,30 @@ public class EmpBasicController {
     @GetMapping("/")
     public RespPageBean getEmployeeByPage(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10") Integer size, Employee employee, Date[] beginDateScope) {
         return employeeService.getEmployeeByPage(page, size, employee,beginDateScope);
+    }
+    @PostMapping("/")
+    public RespBean addEmp(@RequestBody Employee employee){
+        if (employeeService.addEmp(employee) == 1){
+            return RespBean.ok("添加成功");
+        }else {
+            return RespBean.error("添加失败");
+        }
+    }
+    @PutMapping("/")
+    public RespBean updateEmp(@RequestBody Employee employee){
+        if (employeeService.updateEmp(employee) == 1){
+            return RespBean.ok("更新成功");
+        }else {
+            return RespBean.error("更新失败");
+        }
+    }
+    @DeleteMapping("/{id}")
+    public RespBean deleteEmp(@PathVariable Integer id){
+        if (employeeService.deleteEmpByEid(id) == 1){
+            return RespBean.ok("删除成功");
+        }else {
+            return RespBean.error("删除失败");
+        }
     }
 
 
@@ -72,5 +97,16 @@ public class EmpBasicController {
         return POIUtils.employee2Excel(list);
     }
 
+    @PostMapping("/import")
+    public RespBean importData(MultipartFile file) throws IOException{
+        List<Employee> list = POIUtils.excel2Employee(file,nationService.getAllNations(),politicsstatusService.getAllPoliticsstatus(),departmentService.getAllDepartmentsWithOutChildren(),positionService.getAllPos(),jobLevelService.getAllJobLevels());
+        if (list.size()>0){
+            if(employeeService.addEmps(list) == 1)
+            {
+                return RespBean.ok("上传成功");
+            }
+        }
+        return RespBean.error("上传失败");
+    }
 
 }
